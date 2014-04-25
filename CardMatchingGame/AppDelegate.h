501132d90b7461ba2161823944ae95ee109ace3a
Martin Alonso <martin@Martins-MacBook-Pro.local>
2014-04-24 20:25:50 -0700

    Initial Commit

---
+++ CardMatchingGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardMatchingGame
+//
+//  Created by Martin Alonso on 4/24/14.
+//  Copyright (c) 2014 martalson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardMatchingGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardMatchingGame
+//
+//  Created by Martin Alonso on 4/24/14.
+//  Copyright (c) 2014 martalson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
